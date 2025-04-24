666fceaf7c62bfecd41e8811b0f16264df484b68
221B372 <221B372@juet.co.in>
2025-04-24 11:19:04 +0530

    LAB7

---
+++ LAB4/Eager_solutions/Singleton.java
@@ -0,0 +1,11 @@
+class Singleton {
+    private static final Singleton instance = new Singleton();
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+
+
